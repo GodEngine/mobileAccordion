a4e2f8b419139696b48450eda5d84f25ada404a9
GodEngine <1027157090@qq.com>
2016-05-18 18:16:27 +0800

    添加了水平循环播放的功能

--- js/main.js
+++ js/main.js
@@ -1,86 +1,40 @@
 
 	 ;(function(){
-	 	//动画锁，防止瞎jb滑动的那些人
 	 	var lock = true;
 
-	 	//常用dom元素集合的缓存，加快处理速度
 		var img_cache = $(".inner"),
 		sliderArea = document.querySelector("#sliderArea"),
-		main_focus = document.querySelector(".main_focus");
-
+		main_focus = document.querySelector(".main_focus"),
+		total = 3;
 		//判断是否为首尾图片，不是的情况下移除当前元素的touch处理事件
 		var ifAnimate = function (ele,arg){
+/*    		var nextOne = ele.nextSibling,
+    		previousOne = ele.previousSibling;
+
+    		nextOne = nextOne.nodeType==3?nextOne.nextSibling:nextOne;
+    		previousOne = previousOne.nodeType==3?previousOne.previousSibling:previousOne;*/
     		var nextOne = ele.nextElementSibling,
 			previousOne = ele.previousElementSibling;
 
-			//判断是首尾元素时返回false，屏蔽对应的上滑下滑事件
     		if(!previousOne && arg) {
     			return false;
     		}else if(!nextOne && !arg){
     			return false;
     		};
 
-    		//非首位元素则依次先移除active类再向下/上一个将添加active类，并返回true
     		ele.classList.remove("active");
-    		handler.eventRemove.call(ele,"touchendVertical");
+    		handler.eventRemove.call(ele,"Vertical");
 
     		arg?previousOne.classList.add("active"):nextOne.classList.add("active");
     		var blindObj = document.querySelector(".active");
-    		handler.eventBind.call(blindObj,"touchendVertical");
+    		handler.eventBind.call(blindObj,"Vertical");
     		return true;
         };
 
-	 	//事件处理对象，包括touch事件处理、监听和移除
-		var handler = {
-			"touchstart": function(e){
-	    		e.stopPropagation();
-				e.preventDefault();					//屏蔽微信网页内下拉时烦人的“xx内核提供”
-	    		this.start_.X = e.touches[0].screenX;
-	    		this.start_.Y = e.touches[0].screenY;
-	    	},
-	    	"touchmove": function(e){
-	    		e.stopPropagation();
-				e.preventDefault();
-	    		this.end_.X = e.touches[0].screenX;
-	    		this.end_.Y = e.touches[0].screenY;
-	    	},
-	    	"touchendVertical": function(e){       	  //水平方向上的滑动
-	    		e.stopPropagation();
-				e.preventDefault();
-				if(!lock)return;
-	    		if (this.start_.Y < this.end_.Y) {  			
-					animate._AnimateDown(this);
-				} else {
-					animate._AnimateUp(this);
-				}
-	    	},
-	    	"touchendHorizontal": function(e){    	  //垂直方向上的滑动
-	    		e.stopPropagation();
-				e.preventDefault();
-
-	    		if (this.start_.X < this.end_.X) {  			
-					animate._AnimateRight.call(this);
-				} else {
-					animate._AnimateLeft.call(this);
-				}
-	    	},
-	    	"eventBind": function(direction){			//监听touch事件
-		    	this.start_={},this.end_={};
-		    	this.addEventListener('touchstart',handler.touchstart, false);
-		    	this.addEventListener('touchmove',handler.touchmove, false);
-		    	this.addEventListener('touchend',handler[direction], false);
-	    	},
-	    	"eventRemove": function(direction){			//移除元素上绑定的对应touch事件处理句柄
-		    	this.removeEventListener('touchstart',handler.touchstart,false);
-		   		this.removeEventListener('touchmove',handler.touchmove, false);
-		    	this.removeEventListener('touchend',handler[direction], false);
-	    	}
-		};
-
 		//动画处理对象，包括当前元素上滑动与下滑动，整体元素的上移
 		var animate = {
-			"index": 0,				//存储水平滑动时的translate3d值
-			"_AnimateUp": function(obj){						//上滑事件的处理句柄
+			"index": 0,
+			"_AnimateUp": function(obj){
 				if(!ifAnimate(obj,false))return;
 				lock=false;
 				sliderArea.classList.contains("sliderArea_")?undefined:sliderArea.classList.add("sliderArea_");
@@ -98,7 +52,7 @@
 		        'ease-out');
 		        lock = true;
 			},
-			"_AnimateDown": function(obj){						//下滑事件的处理句柄
+			"_AnimateDown": function(obj){
 				if(!ifAnimate(obj,true))return;
 				lock=false;
 				sliderArea.classList.contains("sliderArea_")?sliderArea.classList.remove("sliderArea_"):undefined;
@@ -116,33 +70,95 @@
 		        'ease-out');
 		        lock = true;
 			},
-			"_AnimateRight": function(){					//右滑事件的处理句柄，
-				if (animate.index==10.8) {					//参考swiper处理loop的方法
-					animate.index=-10.8;
-				}else{
-					animate.index = parseFloat((animate.index+10.8).toFixed(1));
-				};				
-				main_focus.style.transform = "translate3d("+animate.index+"rem, 0rem, 0rem)";
+			"_AnimateRight": function(){
+				var right = ((++animate.index)*10.8).toFixed(1);	
+
+				main_focus.style.transform = "translate3d("+right+"rem, 0rem, 0rem)";
+				main_focus.style.transitionDuration = "300ms";
 			},
-			"_AnimateLeft": function(){						//左滑事件的处理句柄
-				if (animate.index==-10.8) {
-					animate.index=10.8;
-				}else{
-					animate.index = parseFloat((animate.index-10.8).toFixed(1));
-				};		
-				main_focus.style.transform = "translate3d("+animate.index+"rem, 0rem, 0rem)";
-			},
+			"_AnimateLeft": function(){
+				var left = ((--animate.index)*10.8).toFixed(1);
+
+				main_focus.style.transform = "translate3d("+left+"rem, 0rem, 0rem)";
+				main_focus.style.transitionDuration = "300ms";
+			}
 
 		};
 
-        $(function(){
-			//垂直方向上的手风琴
-			var active = document.querySelector(".active");				
-			handler.eventBind.call(active,"touchendVertical");
+	 	//事件处理对象，包括touch事件处理、监听和移除
+		var handler = {
+			"touchstart": function(e){
+				e.preventDefault();
+	    		this.start_.X = e.touches[0].screenX;
+	    		this.start_.Y = e.touches[0].screenY;
+	    	},
+	    	"touchmoveVertical": function(e){
+				e.preventDefault();
 
-			//水平方向上循环显示的轮播图
+	    		this.end_.X = e.touches[0].screenX;
+	    		this.end_.Y = e.touches[0].screenY;
+	    	},
+	    	"touchmoveHorizontal": function(e){
+				e.preventDefault();
+
+	    		this.end_.X = e.touches[0].screenX;
+	    		this.end_.Y = e.touches[0].screenY;
+
+	    		if (animate.index==0 && this.start_.X < this.end_.X) {
+	    			animate.index=-total;
+	    			main_focus.style.transform = "translate3d("+(-total*10.8)+"rem, 0rem, 0rem)";
+	    			main_focus.style.transitionDuration = "0ms";
+	    		}else if(animate.index==-total+1 && this.start_.X > this.end_.X){
+	    			animate.index=1;
+	    			main_focus.style.transform = "translate3d("+10.8+"rem, 0rem, 0rem)";
+	    			main_focus.style.transitionDuration = "0ms";
+	    		}
+	    	},
+	    	"touchendVertical": function(e){
+				e.preventDefault();
+
+				if(!lock)return;
+	    		if (this.start_.Y < this.end_.Y) {  			
+					animate._AnimateDown(this);
+				} else {
+					animate._AnimateUp(this);
+				}
+	    	},
+	    	"touchendHorizontal": function(e){
+				e.preventDefault();
+
+	    		if (this.start_.X < this.end_.X) {  			
+					animate._AnimateRight.call(this);
+				} else {
+					animate._AnimateLeft.call(this);
+				}
+	    	},
+	    	"eventBind": function(direction){
+		    	this.start_={},this.end_={};
+		    	this.addEventListener('touchstart',handler.touchstart, false);
+		    	this.addEventListener('touchmove',handler["touchmove"+direction], false);
+		    	this.addEventListener('touchend',handler["touchend"+direction], false);
+	    	},
+	    	"eventRemove": function(direction){
+		    	this.removeEventListener('touchstart',handler.touchstart,false);
+		   		this.removeEventListener('touchmove',handler["touchmove"+direction], false);
+		    	this.removeEventListener('touchend',handler["touchend"+direction], false);
+	    	}
+		};
+
+
+        $(function(){
+			var mySwiper = new Swiper ('.swiper-container', {	    
+			    pagination: '.swiper-pagination'   
+			 });
+
+			var active = document.querySelector(".active");
+			//handler.eventBind.call(active,"touchendVertical");
+			handler.eventBind.call(active,"Vertical");
+
 			var main_focus = document.querySelector(".main_focus");
-			handler.eventBind.call(main_focus,"touchendHorizontal");
+			//handler.eventBind.call(main_focus,"touchendHorizontal");
+			handler.eventBind.call(main_focus,"Horizontal");
 		})
 
 	})();
